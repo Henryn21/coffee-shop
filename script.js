cd260de0694394377f74c469af712b6961b2d397
Henryn21 <45953538+Henryn21@users.noreply.github.com>
2021-03-28 22:57:28 -0700

    logo and nav menu
    updated logo to original transparent one, nav now fills with menu when large enough

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ window.onscroll= function(){
 };
 //resize logo to 200/170
 function scrollSmall(){
-        console.log(window.innerWidth);
         if(document.body.scrollTop>50 || document.documentElement.scrollTop>50){
             document.querySelector("#home").style.width="170px";
             document.querySelector("#home").style.paddingTop= "8px";
@@ -43,7 +42,6 @@ function scrollSmall(){
 let isBelow;
 //resize logo to 300/200
 function scrollBig(){
-    console.log(window.innerWidth);
     if(document.body.scrollTop>50 || document.documentElement.scrollTop>50){
         document.querySelector("#home").style.width="200px";
         document.querySelector("#home").style.paddingTop= "4px";
@@ -56,13 +54,17 @@ function scrollBig(){
     }
 
 }
+//fill navbar with dropdown elements at 1000px
+let menu=document.querySelector("#dropDown");
+let menu2=document.querySelector("#menu2");
+
 //media query
 const mediaQuery= window.matchMedia('(min-width: 1000px)');
 //logo element
 const logo=document.querySelector("#home");
 mediaQuery.addListener(resizeLogo);//attach listner to call function
 //if query is true resize large, else shrink
-function resizeLogo(mq){//BUG, grows logo even without being at top!! still broken, re write with actual scroll check, maybe bool for isBelow
+function resizeLogo(mq){
     //if window bigger than 1000{if at top{resize to 300} else resize to 200}
     if(mq.matches){//if wider than 1000
         if(isBelow){
@@ -71,6 +73,7 @@ function resizeLogo(mq){//BUG, grows logo even without being at top!! still brok
         else{
             logo.style.width="300px";
         }
+        menu2.style.display="inline-block";
     }
     else{//if window less than 1000{if at top{resize to 200} else resize to 170}
         if(isBelow){
@@ -79,7 +82,9 @@ function resizeLogo(mq){//BUG, grows logo even without being at top!! still brok
         else{
             logo.style.width="200px";
         }
+        menu2.style.display="none";
     }
 }
 //initial call
 resizeLogo(mediaQuery);
+
